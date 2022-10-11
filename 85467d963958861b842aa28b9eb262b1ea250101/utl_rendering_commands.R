ba680ade573b9f8477dec12cb7649e1049f358d6
dom <liebl.dominik@gmail.com>
2022-10-11 11:51:18 +0200

    upd seed

--- 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
+++ 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
@@ -1,3 +1,5 @@
 library("quarto")
-quarto::quarto_render("85467d963958861b842aa28b9eb262b1ea250101/index.qmd", cache_refresh=TRUE) # defaults to html
-# quarto_render("index.qmd", output_format = "pdf")
+quarto::quarto_render(
+    input         = "85467d963958861b842aa28b9eb262b1ea250101/index.qmd", 
+    cache_refresh = TRUE, 
+    output_format = "html") # defaults to html
