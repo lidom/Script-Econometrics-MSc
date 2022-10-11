3e0c113f9a033cb96bb046e2b9cb3761d14d040b
dom <liebl.dominik@gmail.com>
2022-10-11 11:44:06 +0200

    upd ssinf ch

--- 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
+++ 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
@@ -1,3 +1,3 @@
 library("quarto")
-quarto_render("index.qmd") # defaults to html
-quarto_render("index.qmd", output_format = "pdf")
+quarto::quarto_render("85467d963958861b842aa28b9eb262b1ea250101/index.qmd", cache_refresh=TRUE) # defaults to html
+# quarto_render("index.qmd", output_format = "pdf")
