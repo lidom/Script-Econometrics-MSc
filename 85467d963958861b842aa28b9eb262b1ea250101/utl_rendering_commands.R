570ebab967fabd06eab52cab25b6806fdeefbcd0
dom <liebl.dominik@gmail.com>
2022-10-11 17:15:39 +0200

    upd ssinf ch

--- 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
+++ 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
@@ -1,5 +1,5 @@
 library("quarto")
 quarto::quarto_render(
-    input         = "85467d963958861b842aa28b9eb262b1ea250101/index.qmd", 
+    input         = "index.qmd", 
     cache_refresh = TRUE, 
     output_format = "html") # defaults to html
