ee69e58e062dfa4af672171212a1e3f5f059a652
dom <liebl.dominik@gmail.com>
2022-10-27 11:18:21 +0200

    complete re-render with added cache

--- 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
+++ 85467d963958861b842aa28b9eb262b1ea250101/utl_rendering_commands.R
@@ -1,5 +1,10 @@
+## From within R
+
 library("quarto")
 quarto::quarto_render(
     input         = "index.qmd", 
     cache_refresh = TRUE, 
     output_format = "html") # defaults to html
+
+## From terminal
+quarto render --cache-refresh
